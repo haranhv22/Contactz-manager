a167606d606dcd78eb0e345f2b4ee0569700f2e6
Your Name <you@example.com>
2021-11-01 19:50:13 +0530

    first commit

---
+++ contact-manager/src/components/ContactList.js
@@ -0,0 +1,56 @@
+import React,{ useRef } from "react";
+import { Link } from "react-router-dom";
+import ContactCard from "./ContactCard";
+
+const ContactList = (props) => {
+    const inputEl = useRef("");
+    const deleteContactHandler = (id) => {
+        props.getContactId(id);
+    };
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+            <ContactCard
+                contact={contact}
+                clickHandler={deleteContactHandler}
+                key={contact.id}
+            />
+        );
+    })
+
+    const getSearchTerm = () => {
+        props.searchKeyword(inputEl.current.value);
+    };
+
+    return (
+           <div className="main">
+            <h2>Contact List
+                <button className="ui button orange right">Add contacts</button>
+                </h2>
+            <h2>Contact List
+                <div className="buttoncon">
+                    <Link to="/add">
+                        <button className="ui button orange right">Add contacts</button>
+                    </Link>
+                </div>
+            </h2>
+                        <div className="ui search">
+                <div className="ui icon input">
+                    <input
+                        ref={inputEl}
+                        type="text"
+                        placeholder="Search Contacts"
+                        className="prompt"
+                        value={props.term}
+                        onChange={getSearchTerm} />
+                <i className="search icon" />
+                </div>
+           </div>
+            
+
+            <div className="ui celled list">{renderContactList.length > 0 ? renderContactList : "No contacts available"}</div>
+            </div>
+            
+    );   
+    };
+
+export default ContactList;
